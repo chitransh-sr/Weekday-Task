78881fd356540f4de07cc64dde4a94045b7fdd52
chitransh-sr <chitransh2209@gmail.com>
2024-05-04 14:48:29 +0530

    code optimised

--- src/components/JobApplyCard.jsx
+++ src/components/JobApplyCard.jsx
@@ -1,81 +1,76 @@
-import React from 'react'
+import React from 'react';
 import { Card, CardHeader, Avatar, CardContent, Typography, Button } from '@mui/material';
-import airtable from "../assets/airtable.jpg"
-import tick from "../assets/tick.png"
-import thunder from "../assets/thunder.png"
+import airtable from "../assets/airtable.jpg";
+import tick from "../assets/tick.png";
+import thunder from "../assets/thunder.png";
+
 function JobCard(props) {
   return (
     <div>
-        <Card sx={{maxWidth:320, borderRadius:5}}>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="recipe">
-                <img src={airtable} alt="" />
-              </Avatar>
-            }
-            titleTypographyProps={{
-              fontSize: 14,
-              color:"#6c757d"
-            }}
-            subheaderTypographyProps={{
-              fontSize: 16,
-              color:"#000"
-            }}
-            title={
-              <div>
-                Weekday
-              </div>
-            }
-            subheader={
-              <div>
-                <div>Frontend Engineer</div>
-                <div style={{ fontSize: 12, color: "#000" }}>Bangalore</div>
-              </div>
-            }
-          />
-          <CardContent>
+      <Card sx={{ maxWidth: 320, borderRadius: 5 }}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="company-logo">
+              <img src={airtable} alt="Airtable Logo" />
+            </Avatar>
+          }
+          titleTypographyProps={{
+            fontSize: 14,
+            color: "#6c757d"
+          }}
+          subheaderTypographyProps={{
+            fontSize: 16,
+            color: "#000"
+          }}
+          title="Weekday"
+          subheader={
+            <div>
+              <div>Frontend Engineer</div>
+              <div style={{ fontSize: 12, color: "#000" }}>Bangalore</div>
+            </div>
+          }
+        />
+        <CardContent>
           <div>
-            Estimated Salary: 18 - 35 LPA <span><img src={tick} alt='tick mark' width={20} height={20} /></span>
+            Estimated Salary: 18 - 35 LPA <span><img src={tick} alt="Tick Mark" width={20} height={20} /></span>
           </div>
           <h4>About Company:</h4>
           <Typography paragraph>
-          Feufo specializes in temporary, permanent, contract, and remote staffing across healthcare, manufacturing, IT, finance, and oil & gas industries.
-          We offer tailored solutions for short-term projects, full-time positions, executive searches, and international staffing needs. s...
-
+            Feufo specializes in temporary, permanent, contract, and remote staffing across healthcare, manufacturing, IT, finance, and oil & gas industries. We offer tailored solutions for short-term projects, full-time positions, executive searches, and international staffing needs.
           </Typography>
 
-          <Button variant="contained" 
-          sx={{
-            width:280, 
-            height:50,
-            backgroundColor:"#55efc4", 
-            color:"#000",
-            '&:hover': {
+          <Button variant="contained"
+            sx={{
+              width: 280,
+              height: 50,
               backgroundColor: "#55efc4",
-            },
-          }}
+              color: "#000",
+              '&:hover': {
+                backgroundColor: "#55efc4",
+              },
+            }}
           >
-            <img src={thunder} alt="Thunder Symbol" width={20}/>Easy Apply
+            <img src={thunder} alt="Thunder Symbol" width={20} />Easy Apply
           </Button>
 
-          <Button variant="contained" 
-          sx={{
-            marginTop:2,
-            height:50,
-            width:280, 
-            backgroundColor:"#4943da", 
-            color:"#fff",
-            '&:hover': {
+          <Button variant="contained"
+            sx={{
+              marginTop: 2,
+              height: 50,
+              width: 280,
               backgroundColor: "#4943da",
-            },
-          }}
+              color: "#fff",
+              '&:hover': {
+                backgroundColor: "#4943da",
+              },
+            }}
           >
-            <img src={thunder} alt="Thunder Symbol" width={20}/>Unlock Referal asks
+            <img src={thunder} alt="Thunder Symbol" width={20} />Unlock Referral Asks
           </Button>
-          </CardContent>
-        </Card>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
 
 export default JobCard;
